3932d085939d81a375f88df9ab09b4369af2998f
SpiderTech <odohfriday33@yahoo.com>
2021-10-13 02:22:26 +0100

    fix bug

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,56 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState } from "react"
 import ICON_MOON from "../assets/images/icon-moon.svg"
 import ICON_SUN from "../assets/images/icon-sun.svg"
 import ICON_SEARCH from "../assets/images/icon-search.svg"
+import LOADER from "../assets/images/loader.gif"
+import { BsGithub } from "react-icons/bs"
 
 import UserDetails from "./UserDetails"
+import axios from "axios"
 
 const Home = () => {
     const [darkMode, setDarkMode] = useState(false)
     const [theme, setTheme] = useState('')
-    const darkRef = useRef()
+    const [searching, setSearching] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState('')
+    const [username, setUsername] = useState('')
+    const [userData, setUserData] = useState(null)
 
     const toggleTheme = () => {
         setDarkMode(!darkMode)
     }
+    const getGithubUser = async () => {
+        try {
+           const res = await axios.get(`https://api.github.com/users/${username}`)
+           setUserData(res.data)
+           setSearching(false)
+           setLoading(false)
+        } catch (error) {
+            console.log(error.message)
+            setLoading(false)
+            setError(error.message)
+        }
+    }
+    const handleSearch = () => {
+       if (!username) {
+           setError('Please enter username')
+       }else{
+           setError('')
+           setLoading(true)
+           setSearching(true)
+           getGithubUser()
+           setLoading(true)
+           setUsername('')
+       }
+    }
+
     useEffect(() => {
          darkMode ? setTheme('dark') : setTheme('light')
     }, [darkMode])
 
     return (
-        <section className={`content ${theme}`} ref={darkRef}>
+        <section className={`content ${theme}`}>
             <div className="main">
                 <header>
                     <h2 className="title">devfinder</h2>
@@ -35,12 +67,42 @@ const Home = () => {
                             
                             <img src={ICON_SEARCH} alt="search-icon" />
                         </div>
-                        <input type="text" placeholder="Search Github username"/>
-                        <a href="#" className="btn"> Search </a>
+                        <input type="text" 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder="Search Github username"/>
+                        <button onClick={handleSearch} className="btn"> Search </button>
                     </div>
                 </div>
+                {
+
+                }
             {
-                <UserDetails/>
+                searching && (
+                    <div className="placeholder">
+                        <div className="icon">
+                            <BsGithub/>
+                        </div>
+                        GiTHUB FINDER
+                        {
+                            loading && (
+                                <div className="loader">
+                                    <img src={LOADER} alt="loader" />
+                                </div>
+                            )
+                        }
+                        {
+                            error && (
+                                <div className="error">
+                                    {error === 'Request failed with status code 404' ? 'Not Found': error}
+                                </div>
+                            )
+                        }
+                    </div>
+                )
+            }
+            {!searching && !loading && userData &&
+                <UserDetails user={userData}/>
             }
             </div>
         </section>
--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,47 +1,50 @@
-import AVATAR from "../assets/images/avatar.png"
+// import AVATAR from "../assets/images/avatar.png"
 import LOCATION_ICON from "../assets/images/icon-location.svg"
 import WEBSITE_ICON from "../assets/images/icon-website.svg"
 import TWITTER_ICON from "../assets/images/icon-twitter.svg"
 import COMPANY_ICON from "../assets/images/icon-company.svg"
+import Moment from "react-moment"
 
-const UserDetails = () => {
+const UserDetails = ({user}) => {
     return (
         <div className="user-profile">
             <aside className="left-aside">
-                <img className="avatar" src={AVATAR} alt="avatar" />
+                <img className="avatar" src={user.avatar_url} alt="avatar" />
             </aside>
          <aside className="right-aside">
             <article className="about-me">
                 <div className="left">
-                    <img className="avatar" src={AVATAR} alt="avatar" />
+                    <img className="avatar" src={user.avatar_url} alt="avatar" />
                 </div>
                 <div className="right">
                     <div className="title">
                         <div className="sub-info">
-                            <h2 className="name">The Octocat</h2>
-                            <span className="username">@octocat</span>
+                            <h2 className="name">{user.name}</h2>
+                            <span className="username">@{user.login}</span>
                         </div>
-                        <span className="joined">Joined 25 Jan 2021</span>
+                        <span className="joined">Joined <Moment date={user.created_at} format="DD MMM Y"/> </span>
                     </div>
                 </div>
             </article>
             <p className="some-text">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio.
-                Quisque volutpat mattis eros.
+                {
+                    user.bio ? user.bio : 'No bio'
+                }
+                
             </p>
             <article className="attributes">
                 <ul>
                     <li>
                         <span className="title">Repos</span>
-                        <h2 className="total">8</h2>
+                        <h2 className="total">{user.public_repos}</h2>
                     </li>
                     <li>
                         <span className="title">Followers</span>
-                        <h2 className="total">3423</h2>
+                        <h2 className="total">{user.followers}</h2>
                     </li>
                     <li>
                         <span className="title">Following</span>
-                        <h2 className="total">9</h2>
+                        <h2 className="total">{user.following}</h2>
                     </li>
                 </ul>
             </article>
@@ -50,19 +53,19 @@ const UserDetails = () => {
                     <li>
                         <div className="contact-logo">
                             <img src={LOCATION_ICON} alt="contact-logo" />
-                            <span className="info">San Francisco</span>
+                            <span className="info">{user.location ? user.location : 'Not Available'}</span>
                         </div>
                     </li>
                     <li>
                         <div className="contact-logo">
                             <img src={WEBSITE_ICON} alt="contact-logo" />
-                            <span className="info">https:github.com.blog</span>
+                            <a className="info" target="_blank" href={user.html_url}>view profile</a>
                         </div>
                     </li>
                     <li>
                         <div className="contact-logo">
                             <img src={TWITTER_ICON} alt="contact-logo" />
-                            <span className="info">Not Available</span>
+                            <span className="info">{user.twitter_username ? user.twitter_username : 'Not Available'}</span>
                         </div>
                     </li>
                     <li>
